871a3242c5f4eb462d696f98961f54102d4e237c
Wu Qizhen <3162946784@qq.com>
2026-03-12 21:57:24 +0800

    细节优化

--- app/src/main/java/com/codeintellix/envlink/activity/common/screen/EnvLinkPage.kt
+++ app/src/main/java/com/codeintellix/envlink/activity/common/screen/EnvLinkPage.kt
@@ -21,7 +21,9 @@ import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -382,10 +384,10 @@ fun EnvLinkPage() {
         if (showRoomMenu) {
             Box(
                 modifier = Modifier
-                    .fillMaxSize()
                     .clickVfx(onClick = {
                         showRoomMenu = false
                     })
+                    .fillMaxSize()
             )
         }
     }
